1e84af3f432a1d9899b314aaf12eddd533f68d0b
Basharyam <Bshar.yamin@gmail.com>
2025-05-14 16:23:27 +0300

    Update King.cpp

--- Chess/src/King.cpp
+++ Chess/src/King.cpp
@@ -3,25 +3,14 @@
 #include <cctype>
 
 bool King::is_legel_movement(const string &input, const string &board, bool isWhiteTurn) {
-    int col_source = input[0] - 'a';
-    int row_source = 8 - (input[1] - '0');
-    int col_dest = input[2] - 'a';
-    int row_dest = 8 - (input[3] - '0');
-    int rowDiff = abs(row_source - row_dest);
-    int colDiff = abs(col_source - col_dest);
-    // King moves only 1 square in any direction
-    if (rowDiff > 1 || colDiff > 1)
+    int srcCol = input[0] - 'a';
+    int srcRow = 8 - (input[1] - '0');
+    int dstCol = input[2] - 'a';
+    int dstRow = 8 - (input[3] - '0');
+    if (std::max(std::abs(srcRow - dstRow), std::abs(srcCol - dstCol)) > 1)
         return false;
-    char destPiece = board[row_dest * 8 + col_dest];
-    if (destPiece == '#')
+    char dstPiece = board[dstRow * 8 + dstCol];
+    if (dstPiece == '#')
         return true;
-    if (isWhiteTurn && isupper(destPiece))
-        return false;
-    if (!isWhiteTurn && islower(destPiece))
-        return false;
-    return true;
-}
-
-char King::get_type() {
-    return type;
+    return isWhiteTurn ? !std::isupper(dstPiece) : !std::islower(dstPiece);
 }
