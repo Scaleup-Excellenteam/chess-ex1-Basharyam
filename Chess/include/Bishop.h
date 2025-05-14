6ac6030c9e26a07e7b0d40f211b483db2a7ff4a2
Basharyam <Bshar.yamin@gmail.com>
2025-05-14 15:58:15 +0300

    Update Bishop.h

--- Chess/include/Bishop.h
+++ Chess/include/Bishop.h
@@ -3,12 +3,10 @@
 
 #include "Piece.h"
 class Bishop : public Piece{
-private :
-    char type;
 public:
     Bishop(char t) : type(t) {}
     bool is_legel_movement(const std::string &input, const std::string &board, bool isWhiteTurn) override;
-    char get_type() override;
+    
 
 
 };
