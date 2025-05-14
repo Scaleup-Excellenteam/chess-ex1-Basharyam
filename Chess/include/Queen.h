13693ecff6feab8e9336d7fcdc749e84c5f35566
Basharyam <Bshar.yamin@gmail.com>
2025-05-14 16:06:18 +0300

    Update Queen.h

--- Chess/include/Queen.h
+++ Chess/include/Queen.h
@@ -3,13 +3,10 @@
 #include "Piece.h"
 
 class Queen : public Piece{
-private:
-    char type ;
-
 public:
-    bool is_legel_movement(const std::string &input, const std::string &board, bool isWhiteTurn) override;
-    char get_type() override;
     Queen(char t) : type(t) {}
+    bool is_legel_movement(const std::string &input, const std::string &board, bool isWhiteTurn) override;
+    
 };
 
 
