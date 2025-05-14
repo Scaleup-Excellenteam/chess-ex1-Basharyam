0bd8569e9e26bcbcb482526d9713d3f39a091e55
Basharyam <Bshar.yamin@gmail.com>
2025-05-14 16:03:36 +0300

    Update King.h

--- Chess/include/King.h
+++ Chess/include/King.h
@@ -1,16 +1,10 @@
 #ifndef CHESS_KING_H
 #define CHESS_KING_H
 #include "Piece.h"
-
-#include "Piece.h"
-
 class King : public Piece {
-private:
-    char type;
 public:
     King(char t) : type(t) {}
     bool is_legel_movement(const string &input, const string &board, bool isWhiteTurn) override;
-    char get_type() override;
 };
 
 
